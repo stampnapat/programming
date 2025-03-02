e441496ade8f15657fbbab4c07c56326a9e3dc5f
stampnapat <stampnapatt@gmail.com>
2025-03-02 20:32:18 +0700

    Added new files

--- 3-11.py
+++ 3-11.py
@@ -1,5 +1,17 @@
-x = int(input())
+# x = int(input())
+# if x < 0 :
+#     print(-(x%3), -(x%11))
+# else :
+#     print(x%3, x%11)
 
-print(x%3 ,x%11)
 
 
+def  mod(n,m):	    
+    result = 0	
+    for i in range(len(n)):
+        print(i)
+        result = (result * 10 + int(n[i])) % m	    
+        print(f"result{result}")
+    return result	
+n = input()	
+print(mod(n,3),mod(n,11))
---
+++ chem.py
@@ -0,0 +1,24 @@
+Chapter 4 Acid-Base P1-29
+https://youtu.be/0OyrQV1_SoA
+  
+ 
+Chapter5 Ionic Equilibrium P1-20
+https://youtu.be/wY-cd0AinSI
+ Chapter5 Ionic Equilibrium P21-40
+https://youtu.be/gtGeu3NE8Sc
+ Chapter5 Ionic Equilibrium P41-66
+https://youtu.be/RvQvE66rXUg
+ Chapter5 Ionic Equilibrium P67-88
+https://youtu.be/0kK5fcvCqKo
+ Chapter5 Ionic Equilibrium P89-99
+https://youtu.be/02B1M0jFF30
+ Chapter5 Ionic Equilibrium P100-126
+https://youtu.be/UI-sMu6knlk
+ Chapter5 Ionic Equilibrium P127-138
+https://youtu.be/ftvjWo__UX8
+
+
+Summary Ionic equilibrium
+
+
+https://youtu.be/cH4RRHzPFd8
